8da3ba4e5eafd93fe945b6b3552117a34ff7939b
duduhqp <duduhqp@gmail.com>
2022-08-12 13:23:29 -0300

    deletar tecnico

--- src/main/java/com/epeniche/helpdesk/services/TecnicoService.java
+++ src/main/java/com/epeniche/helpdesk/services/TecnicoService.java
@@ -49,6 +49,15 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	
+	public void delete(Integer id) {
+		Tecnico obj =  findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}else {
+			repository.deleteById(id);
+		}
+	}
 
 	private void validaPorCPFEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -62,6 +71,8 @@ public class TecnicoService {
 		}
 		
 	}
+
+	
 
 	
 	
