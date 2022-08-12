091aa214c13be7d6219d73a8a51dbaf6ecf76d59
duduhqp <duduhqp@gmail.com>
2022-08-12 11:42:36 -0300

    excecao para cpf e email ja cadastrados

--- src/main/java/com/epeniche/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/epeniche/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.epeniche.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.epeniche.helpdesk.domain.Pessoa;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
--- src/main/java/com/epeniche/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/epeniche/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.epeniche.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.epeniche.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -21,6 +22,16 @@ public class ResourceExceptionHandler {
 				"Object not Found", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violaçãop de dados", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 	
 }
